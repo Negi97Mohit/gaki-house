cb103c6d36df1a6a4d86c35bc6daadb0c956efe6
Mohit Negi <62002911+Negi97Mohit@users.noreply.github.com>
2025-12-20 08:51:39 +0530

    FIXING: hide the text box thingie

--- src/components/layouts/dynamic/core/EditableText.tsx
+++ src/components/layouts/dynamic/core/EditableText.tsx
@@ -3,39 +3,51 @@ import { useDynamicLayout } from "./DynamicLayoutContext";
 import { cn } from "@/lib/utils";
 
 export interface EditableTextProps {
-    sectionId?: string;
-    fieldId?: string;
-    defaultValue?: string;
-    className?: string;
-    multiline?: boolean;
-    style?: React.CSSProperties;
+  sectionId?: string;
+  fieldId?: string;
+  defaultValue?: string;
+  className?: string;
+  multiline?: boolean;
+  style?: React.CSSProperties;
 }
 
 export const EditableText: React.FC<EditableTextProps> = ({
-    sectionId = 'default',
-    fieldId = 'text',
-    defaultValue = '',
-    className,
-    multiline = false,
-    style
+  sectionId = "default",
+  fieldId = "text",
+  defaultValue = "",
+  className,
+  multiline = false,
+  style,
 }) => {
-    const { layout, editor, colors } = useDynamicLayout();
+  const { layout, editor, colors, controlsVisible } = useDynamicLayout();
 
-    const uniqueId = `${sectionId}_${fieldId}`;
-    const value = layout.customSectionData?.[sectionId]?.[fieldId] ?? defaultValue;
-    const combinedStyle = editor.getFieldStyle(uniqueId, { ...style, color: colors.textColor });
+  const uniqueId = `${sectionId}_${fieldId}`;
+  const value =
+    layout.customSectionData?.[sectionId]?.[fieldId] ?? defaultValue;
+  const combinedStyle = editor.getFieldStyle(uniqueId, {
+    ...style,
+    color: colors.textColor,
+  });
 
-    const commonProps = {
-        value,
-        onChange: (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => editor.handleUpdateText(sectionId, fieldId, e.target.value),
-        onFocus: (e: React.FocusEvent<HTMLElement>) => editor.handleFocus(uniqueId, e),
-        style: combinedStyle,
-        className: cn("bg-transparent border-none focus:outline-none w-full pointer-events-auto", className),
-    };
+  const commonProps = {
+    value,
+    onChange: (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) =>
+      editor.handleUpdateText(sectionId, fieldId, e.target.value),
+    onFocus: (e: React.FocusEvent<HTMLElement>) =>
+      editor.handleFocus(uniqueId, e),
+    style: combinedStyle,
+    className: cn(
+      "bg-transparent border-none focus:outline-none w-full pointer-events-auto transition-all duration-300",
+      // Hide resize handle and scrollbar when controls are not visible
+      !controlsVisible &&
+        "resize-none [scrollbar-width:none] [-ms-overflow-style:none] [&::-webkit-scrollbar]:hidden",
+      className
+    ),
+  };
 
-    if (multiline) {
-        return <textarea rows={1} {...commonProps} />;
-    }
+  if (multiline) {
+    return <textarea rows={1} {...commonProps} />;
+  }
 
-    return <input {...commonProps} />;
+  return <input {...commonProps} />;
 };
